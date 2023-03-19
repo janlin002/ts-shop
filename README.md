6672628b130f10372dd25dcbcb8c55cc5ea069fb
janlin002 <123davidbill@gmail.com>
2023-03-19 23:03:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ts-shop
+
+使用 TypeScript 去開發一個購物網站
---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  console.log('123123')
+  return (
+    <div>app</div>
+  )
+}
+
+export default App
