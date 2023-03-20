5fd86ea6b65342a3add595712541f94a796b6c8c
janlin002 <123davidbill@gmail.com>
2023-03-20 10:11:00 +0800

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: './src/index.tsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/' // 讓webpack了解該去哪看更新
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: [
-          {
-            directory: path.join(__dirname),
-          },
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/ // 排除編譯
-            },
-            {
-                test: /\.(jsx|tsx)x?$/,    // add |ts
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                },
-              },
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    }
-}
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/', // 讓webpack了解該去哪看更新
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/, // 排除編譯
+      },
+      {
+        test: /\.(jsx|tsx)x?$/, // add |ts
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+};
